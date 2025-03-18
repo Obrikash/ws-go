e7dc422764c8dea0b6bea615e94107b2377fea7d
Nikita <obrikash@gmail.com>
2025-03-18 11:58:52 +0300

    login/logout endpoints

--- server/router/router.go
+++ server/router/router.go
@@ -9,11 +9,13 @@ import (
 var r *gin.Engine
 
 func InitRouter(userHandler *user.Handler) {
-    r = gin.Default()
+	r = gin.Default()
 
-    r.POST("/signup", userHandler.CreateUser)
+	r.POST("/signup", userHandler.CreateUser)
+	r.POST("/login", userHandler.Login)
+	r.GET("/logout", userHandler.Logout)
 }
 
 func Start(addr string) error {
-    return r.Run(addr)
+	return r.Run(addr)
 }
